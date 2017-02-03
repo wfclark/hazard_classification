9e81863ff14788f9b277c8bfe778d28c733edb68
William Clark <wfclark2@gmail.com>
2017-02-03 13:17:14 -0500

    Update exposure.py

--- exposure.py
+++ exposure.py
@@ -42,10 +42,21 @@ arcpy.AddField_management(buildings,"PerSLR3", "Double")
 
 #start of slr and cat analysis for field classifcation of exposure
 
-buildings_fields = ["FID", "SLR"]
-temp_fields = ["FID_allbui"]
+buildings_fields = ["FID", "SLR", "SLRarea", "PerSLR3"]
+temp_fields = ["FID_allbui", "SLRarea"]
+
+
+
+
+
+
 
 arcpy.Intersect_analysis([buildings,slr5], slr_temp5) 
+
+with arcpy.da.UpdateCursor(slr_temp5, ["SHAPE@AREA", "SLRarea"]) as cursor:
+    for row in cursor:
+        row[1] = row[0]
+        cursor.updateRow(row)
 
 cur = arcpy.da.SearchCursor(slr_temp5, temp_fields)
 for row in cur:
@@ -55,7 +66,25 @@ for row in cur:
 			row2[1] = '5'
 		cur2.updateRow(row2)
 
-arcpy.Delete_management("slr_temp5")  
+cur = arcpy.da.SearchCursor(slr_temp5, temp_fields)
+for row in cur:
+	cur2 = arcpy.da.UpdateCursor(buildings, buildings_fields)
+	for row2 in cur2:
+		if row[0] == row2[0]:
+			row2[2] = row[1]
+		cur2.updateRow(row2)
+
+
+
+
+
+
+
+
+
+
+
+
 
 arcpy.Intersect_analysis([buildings,slr4], slr_temp4) 
 
@@ -67,7 +96,27 @@ for row in cur:
 			row2[1] = '4'
 		cur2.updateRow(row2)
 
-arcpy.Delete_management("slr_temp4")  
+with arcpy.da.UpdateCursor(slr_temp4, ["SHAPE@AREA", "SLRarea"]) as cursor:
+    for row in cursor:
+        row[1] = row[0]
+        cursor.updateRow(row)
+
+cur = arcpy.da.SearchCursor(slr_temp4, temp_fields)
+for row in cur:
+	cur2 = arcpy.da.UpdateCursor(buildings, buildings_fields)
+	for row2 in cur2:
+		if row[0] == row2[0]:
+			row2[2] = row[1]
+		cur2.updateRow(row2)
+
+
+
+
+
+
+
+
+
 
 arcpy.Intersect_analysis([buildings,slr3], slr_temp3) 
 
@@ -79,7 +128,27 @@ for row in cur:
 			row2[1] = '3'
 		cur2.updateRow(row2)
 
-arcpy.Delete_management("slr_temp3")  
+with arcpy.da.UpdateCursor(slr_temp3S, ["SHAPE@AREA", "SLRarea"]) as cursor:
+    for row in cursor:
+        row[1] = row[0]
+        cursor.updateRow(row)
+        
+cur = arcpy.da.SearchCursor(slr_temp3, temp_fields)
+for row in cur:
+	cur2 = arcpy.da.UpdateCursor(buildings, buildings_fields)
+	for row2 in cur2:
+		if row[0] == row2[0]:
+			row2[2] = row[1]
+		cur2.updateRow(row2)
+
+
+
+
+
+
+
+
+
 
 arcpy.Intersect_analysis([buildings,slr2], slr_temp2) 
 
@@ -91,7 +160,23 @@ for row in cur:
 			row2[1] = '2'
 		cur2.updateRow(row2)
 
-arcpy.Delete_management("slr_temp2")  
+
+with arcpy.da.UpdateCursor(slr_temp2, ["SHAPE@AREA", "SLRarea"]) as cursor:
+    for row in cursor:
+        row[1] = row[0]
+        cursor.updateRow(row)
+        
+cur = arcpy.da.SearchCursor(slr_temp2, temp_fields)
+for row in cur:
+	cur2 = arcpy.da.UpdateCursor(buildings, buildings_fields)
+	for row2 in cur2:
+		if row[0] == row2[0]:
+			row2[2] = row[1]
+		cur2.updateRow(row2)
+
+
+
+
 
 arcpy.Intersect_analysis([buildings,slr1], slr_temp1) 
 
@@ -103,10 +188,34 @@ for row in cur:
 			row2[1] = '1'
 		cur2.updateRow(row2)
 
+
+with arcpy.da.UpdateCursor(slr_temp1, ["SHAPE@AREA", "SLRarea"]) as cursor:
+    for row in cursor:
+        row[1] = row[0]
+        cursor.updateRow(row)
+        
+cur = arcpy.da.SearchCursor(slr_temp1, temp_fields)
+for row in cur:
+	cur2 = arcpy.da.UpdateCursor(buildings, buildings_fields)
+	for row2 in cur2:
+		if row[0] == row2[0]:
+			row2[2] = row[1]
+		cur2.updateRow(row2)
+
+
 arcpy.Delete_management("slr_temp1")  
+arcpy.Delete_management("slr_temp2")  
+arcpy.Delete_management("slr_temp3")  
+arcpy.Delete_management("slr_temp4")  
+arcpy.Delete_management("slr_temp5")  
 
 
 
+
+
+
+
+		
 arcpy.Intersect_analysis([buildings,cat5], cat_temp5) 
 
 cur = arcpy.da.SearchCursor(cat_temp5, temp_fields)
@@ -117,11 +226,9 @@ for row in cur:
 			row2[1] = '5'
 		cur2.updateRow(row2)
 
-arcpy.Delete_management("cat_temp5")  
-
-
 
 arcpy.Intersect_analysis([buildings,cat4], cat_temp4) 
+
 
 cur = arcpy.da.SearchCursor(cat_temp4, temp_fields)
 for row in cur:
@@ -131,8 +238,8 @@ for row in cur:
 			row2[1] = '4'
 		cur2.updateRow(row2)
 
-arcpy.Delete_management("cat_temp4")  
 
+arcpy.Intersect_analysis([buildings,cat3], cat_temp3) 
 
 cur = arcpy.da.SearchCursor(cat_temp3, temp_fields)
 for row in cur:
@@ -141,9 +248,7 @@ for row in cur:
 		if row[0] == row2[0]:
 			row2[1] = '3'
 		cur2.updateRow(row2)
-
-arcpy.Delete_management("cat_temp3")  
-
+		
 
 arcpy.Intersect_analysis([buildings,cat2], cat_temp2) 
 
@@ -155,9 +260,6 @@ for row in cur:
 			row2[1] = '2'
 		cur2.updateRow(row2)
 
-arcpy.Intersect_analysis([buildings,cat3], cat_temp3) 
-
-arcpy.Delete_management("cat_temp2")  
 
 arcpy.Intersect_analysis([buildings,cat1], cat_temp1) 
 
@@ -171,4 +273,10 @@ for row in cur:
 
 
 arcpy.Delete_management("cat_temp1")  
+arcpy.Delete_management("cat_temp2")  
+arcpy.Delete_management("cat_temp3")  
+arcpy.Delete_management("cat_temp4")  
+arcpy.Delete_management("cat_temp5")  
 
+
+#Begin clip shape analysis
