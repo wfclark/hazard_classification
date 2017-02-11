121d28b79f155c5f16f50e3117d2fd24fe6d3b3f
William Clark <wfclark2@gmail.com>
2017-02-10 21:06:46 -0500

    Update exposure.py

--- exposure.py
+++ exposure.py
@@ -1,46 +1,88 @@
 import arcpy
 from arcpy import env  
 
-env.workspace = r"C:/users/wfcla/Desktop/Classification_Automation"  
-
+env.workspace = r"C:/users/wfcla/Desktop/Classification_Automation/data.gdb"  
 env.overwriteOutput = True  
   
-multipatch = "AllBuildings"
-buildings = "building_footprints"
+multipatch = arcpy.GetParameterAsText(0)
 
-slr1 = "SLR1"
-slr2 = "SLR2"
-slr3 = "SLR3"
-slr4 = "SLR4"
-slr5 = "SLR5"
+buildings = arcpy.CreateScratchName("building_footprints",
+                                       data_type="Shapefile",
+                                       workspace=arcpy.env.scratchFolder)
 
-cat1 = "Cat1"
-cat2 = "Cat2"
-cat3 = "Cat3"
-cat4 = "Cat4"
-cat5 = "Cat5"
+slr1 = arcpy.GetParameterAsText(1)
+slr2 = arcpy.GetParameterAsText(2)
+slr3 = arcpy.GetParameterAsText(3)
+slr4 = arcpy.GetParameterAsText(4)
+slr5 = arcpy.GetParameterAsText(5)
 
-slr_temp1 = 'slr_temp1'
-slr_temp2 = 'slr_temp2'
-slr_temp3 = 'slr_temp3'
-slr_temp4 = 'slr_temp4'
-slr_temp5 = 'slr_temp5'
+cat1 = arcpy.GetParameterAsText(6)
+cat2 = arcpy.GetParameterAsText(7)
+cat3 = arcpy.GetParameterAsText(8)
+cat4 = arcpy.GetParameterAsText(9)
+cat5 = arcpy.GetParameterAsText(10)
 
-cat_temp1 = 'cat_temp1'
-cat_temp2 = 'cat_temp2'
-cat_temp3 = 'cat_temp3'
-cat_temp4 = 'cat_temp4'
-cat_temp5 = 'cat_temp5'
+slr_temp1 = arcpy.CreateScratchName("temp1",
+                                       data_type="Shapefile",
+                                       workspace=arcpy.env.scratchFolder)
+slr_temp2 = arcpy.CreateScratchName("temp2",
+                                       data_type="Shapefile",
+                                       workspace=arcpy.env.scratchFolder)
 
-arcpy.MultiPatchFootprint_3d(multipatch, buildings) 
+slr_temp3 = arcpy.CreateScratchName("temp3",
+                                       data_type="Shapefile",
+                                       workspace=arcpy.env.scratchFolder)
+
+slr_temp4 = arcpy.CreateScratchName("temp4",
+                                       data_type="Shapefile",
+                                       workspace=arcpy.env.scratchFolder)
+
+slr_temp5 = arcpy.CreateScratchName("temp5",
+                                       data_type="Shapefile",
+                                       workspace=arcpy.env.scratchFolder)
+
+cat_temp1 = arcpy.CreateScratchName("temp6",
+                                       data_type="Shapefile",
+                                       workspace=arcpy.env.scratchFolder)
+
+cat_temp2 = arcpy.CreateScratchName("temp7",
+                                       data_type="Shapefile",
+                                       workspace=arcpy.env.scratchFolder)
+
+cat_temp3 = arcpy.CreateScratchName("tem8",
+                                       data_type="Shapefile",
+                                       workspace=arcpy.env.scratchFolder)
+
+cat_temp4 = arcpy.CreateScratchName("temp9",
+                                       data_type="Shapefile",
+                                       workspace=arcpy.env.scratchFolder)
+
+cat_temp5 = arcpy.CreateScratchName("temp10",
+                                       data_type="Shapefile",
+                                       workspace=arcpy.env.scratchFolder)
+
+
+arcpy.MultiPatchFootprint_3d(multipatch, buildings)
 
 # Add field allbuildings_footprints 
+
+
+arcpy.AddField_management(multipatch, "Category", "Double")
+arcpy.AddField_management(multipatch, "SLR", "Double")  
+arcpy.AddField_management(multipatch, "SLRarea", "Double")
+arcpy.AddField_management(multipatch,"PerSLR3", "Double")
+arcpy.AddField_management(multipatch,"area", "Double")
+
+
 
 arcpy.AddField_management(buildings, "Category", "Double")
 arcpy.AddField_management(buildings, "SLR", "Double")  
 arcpy.AddField_management(buildings, "SLRarea", "Double")
 arcpy.AddField_management(buildings,"PerSLR3", "Double")
 arcpy.AddField_management(buildings,"area", "Double")
+
+
+
 
 
 buildings_fields = ["FID", "SLR", "SLRarea", "PerSLR3", "Area"]
@@ -55,8 +97,15 @@ with arcpy.da.UpdateCursor(buildings, ["SHAPE@AREA", "area"]) as cursor:
         row[1] = row[0]
         cursor.updateRow(row)
 
+arcpy.Intersect_analysis([buildings, slr5], slr_temp5) 
 
-arcpy.Intersect_analysis([buildings,slr5], slr_temp5) 
+cur = arcpy.da.SearchCursor(slr_temp5, temp_fields)
+for row in cur:
+	cur2 = arcpy.da.UpdateCursor(buildings, buildings_fields)
+	for row2 in cur2:
+		if row[0] == row2[0]:
+			row2[1] = '5'
+		cur2.updateRow(row2)
 
 with arcpy.da.UpdateCursor(slr_temp5, ["SHAPE@AREA", "SLRarea"]) as cursor:
     for row in cursor:
@@ -68,24 +117,12 @@ for row in cur:
 	cur2 = arcpy.da.UpdateCursor(buildings, buildings_fields)
 	for row2 in cur2:
 		if row[0] == row2[0]:
-			row2[1] = '5'
-		cur2.updateRow(row2)
-
-cur = arcpy.da.SearchCursor(slr_temp5, temp_fields)
-for row in cur:
-	cur2 = arcpy.da.UpdateCursor(buildings, buildings_fields)
-	for row2 in cur2:
-		if row[0] == row2[0]:
 			row2[2] = row[1]
 		cur2.updateRow(row2)
 
-cur = arcpy.da.SearchCursor(slr_temp5, temp_fields)
-for row in cur:
-	cur2 = arcpy.da.UpdateCursor(buildings, buildings_fields)
-	for row2 in cur2:
-		if row[0] == row2[0]:
-			row2[2] = row[1]
-		cur2.updateRow(row2)
+
+
+
 
 
 
@@ -157,6 +194,15 @@ for row in cur:
 		cur2.updateRow(row2)
 
 
+
+
+
+
+
+
+
+
+
 arcpy.Intersect_analysis([buildings,slr2], slr_temp2) 
 
 cur = arcpy.da.SearchCursor(slr_temp2, temp_fields)
@@ -180,6 +226,17 @@ for row in cur:
 		if row[0] == row2[0]:
 			row2[2] = row[1]
 		cur2.updateRow(row2)
+
+
+
+
+
+
+
+
+
+
+
 
 
 arcpy.Intersect_analysis([buildings,slr1], slr_temp1) 
@@ -207,11 +264,9 @@ for row in cur:
 		cur2.updateRow(row2)
 
 
-arcpy.Delete_management("slr_temp1")  
-arcpy.Delete_management("slr_temp2")  
-arcpy.Delete_management("slr_temp3")  
-arcpy.Delete_management("slr_temp4")  
-arcpy.Delete_management("slr_temp5")  
+
+
+
 
 		
 arcpy.Intersect_analysis([buildings,cat5], cat_temp5) 
@@ -223,6 +278,8 @@ for row in cur:
 		if row[0] == row2[0]:
 			row2[1] = '5'
 		cur2.updateRow(row2)
+
+
 
 
 arcpy.Intersect_analysis([buildings,cat4], cat_temp4) 
@@ -237,6 +294,9 @@ for row in cur:
 		cur2.updateRow(row2)
 
 
+
+
+
 arcpy.Intersect_analysis([buildings,cat3], cat_temp3) 
 
 cur = arcpy.da.SearchCursor(cat_temp3, temp_fields)
@@ -246,6 +306,9 @@ for row in cur:
 		if row[0] == row2[0]:
 			row2[1] = '3'
 		cur2.updateRow(row2)
+
+
+
 
 
 
@@ -260,6 +323,11 @@ for row in cur:
 		cur2.updateRow(row2)
 
 
+
+
+
+
+
 arcpy.Intersect_analysis([buildings,cat1], cat_temp1) 
 
 cur = arcpy.da.SearchCursor(cat_temp1, temp_fields)
@@ -272,11 +340,57 @@ for row in cur:
 
 
 
-arcpy.Delete_management("cat_temp1")  
-arcpy.Delete_management("cat_temp2")  
-arcpy.Delete_management("cat_temp3")  
-arcpy.Delete_management("cat_temp4")  
-arcpy.Delete_management("cat_temp5")  
+
+arcpy.Delete_management("temp1")
+arcpy.Delete_management("temp2")  
+arcpy.Delete_management("temp3")  
+arcpy.Delete_management("temp4")  
+arcpy.Delete_management("temp5")  
+arcpy.Delete_management("temp6")  
+arcpy.Delete_management("temp7")  
+arcpy.Delete_management("temp8")  
+arcpy.Delete_management("temp9")  
+arcpy.Delete_management("temp10")
+
+
+
+
+
+cur = arcpy.da.SearchCursor(buildings, buildings_fields)
+for row in cur:
+	cur2 = arcpy.da.UpdateCursor(multipatch, buildings_fields)
+	for row2 in cur2:
+		if row[0] == row2[0]:
+			row2[1] = row[1]
+		cur2.updateRow(row2)
+
+cur = arcpy.da.SearchCursor(buildings, temp_fields)
+for row in cur:
+	cur2 = arcpy.da.UpdateCursor(multipatch, buildings_fields)
+	for row2 in cur2:
+		if row[0] == row2[0]:
+			row2[2] = row[2]
+		cur2.updateRow(row2)
+
+cur = arcpy.da.SearchCursor(buildings, temp_fields)
+for row in cur:
+	cur2 = arcpy.da.UpdateCursor(multipatch, buildings_fields)
+	for row2 in cur2:
+		if row[0] == row2[0]:
+			row2[3] = row[3]
+		cur2.updateRow(row2)
+
+cur = arcpy.da.SearchCursor(buildings, temp_fields)
+for row in cur:
+	cur2 = arcpy.da.UpdateCursor(multipatch, buildings_fields)
+	for row2 in cur2:
+		if row[0] == row2[0]:
+			row2[4] = row[4]
+		cur2.updateRow(row2)
+
+
 
 
 #Begin clip shape analysis
+
+
